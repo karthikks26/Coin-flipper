ec706b4748a79ca7dfdab431f667951e9e66c8ed
Karthik ks <karthikks260703@gmail.com>
2024-08-25 21:02:20 +0530

    c1

--- frontend/src/data/constants.jsx
+++ frontend/src/data/constants.jsx
@@ -3,7 +3,8 @@ import { COINFLIP_ABI } from "./coinFlipAbi";
 export const DEFAULT_THEME = "dark";
 export const WAITFOR_CONFIRMATIONS = 2;
 
-export const COINFLIP_ADDRESS = "0x097d039Bb7353B0bcD3d585B5a71e8B575F2f3f7";
+export const COINFLIP_ADDRESS = import.meta.env.VITE_COINFLIP_ADDRESS;
+console.log("d", COINFLIP_ADDRESS);
 
 export const COINFLIP = {
   address: COINFLIP_ADDRESS,
